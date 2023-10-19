47efa9e30ad52c3021027d7cb817031ac7165738
qq2582799774 <2582799774@qq.com>
2023-10-20 01:27:50 +0800

    Site updated: 2023-10-20 01:27:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/10/14/HTML + CSS 实现一个酷炫的夜间模式切换动画/","2023/10/20/shubiaochufa001/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/10/14/HTML + CSS 实现一个酷炫的夜间模式切换动画/","2023/10/20/bizhi0001/","2023/10/20/shubiaochufa001/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
