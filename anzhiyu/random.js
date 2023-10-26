717d32f841d70b189c01770acb63c2c4d2310421
qq2582799774 <2582799774@qq.com>
2023-10-26 21:56:48 +0800

    Site updated: 2023-10-26 21:56:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/10/14/1019codeshare/","2023/10/20/bizhi0001/","2023/10/26/1026mingpian/","2023/10/20/shubiaochufa001/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/10/14/1019codeshare/","2023/10/26/1026mingpian/","2023/10/20/bizhi0001/","2023/10/20/shubiaochufa001/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
